afbee204b99e58b53604b40ce71d1ad7ee7495ea
tomtom99 <tom@fractalweb.co.uk>
2022-12-31 11:20:05 +0000

    add a test chart

---
+++ BlazorApp1/BlazorApp1/wwwroot/chart.js
@@ -0,0 +1,11 @@
+﻿var run = false;
+
+window.setup = (id, config) => {
+    var ctx = document.getElementById(id).getContext('2d');
+    if (!run) {
+        new Chart(ctx, config);
+
+    }
+    run = true;
+    
+}
