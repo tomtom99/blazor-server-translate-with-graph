74019bbdd6edc0f40f63eb86d3a117a492070e83
tomtom99 <tom@fractalweb.co.uk>
2023-01-01 12:20:38 +0000

    charts

--- BlazorApp1/BlazorApp1/wwwroot/chart.js
+++ BlazorApp1/BlazorApp1/wwwroot/chart.js
@@ -1,11 +1,21 @@
 ﻿var run = false;
 
+var chart;
 window.setup = (id, config) => {
     var ctx = document.getElementById(id).getContext('2d');
     if (!run) {
-        new Chart(ctx, config);
+        chart = new Chart(ctx, config);
 
     }
     run = true;
     
+}
+
+function addData(chartID, label, data) {
+    //var chart = document.getElementById(chartID);
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
 }
