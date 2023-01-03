25b5960303c3592660cd487caaefffcc462282c7
tomtom99 <tom@fractalweb.co.uk>
2023-01-03 09:08:03 +0000

    update

---
+++ BlazorApp1/BlazorApp1/obj/Release/net6.0/PubTmp/Out/wwwroot/chart.js
@@ -0,0 +1,53 @@
+﻿var run = false;
+
+var chart;
+window.setup = (id, config) => {
+    var ctx = document.getElementById(id).getContext('2d');
+    if (!run) {
+        chart = new Chart(ctx, config);
+
+    }
+    run = true;
+    
+}
+
+function addData(label, data, label2, data2) {
+    // change this to take a json object.
+    /*  {labels:["one","two"],
+     *   data:[1,2]
+     *   }
+     */
+    // clear data
+    chart.data.labels = [];
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = [];
+    });
+    // add data
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.data.labels.push(label2);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data2);
+    });
+    chart.update();
+}
+function updateChart() {
+    chart.update();
+}
+function removeData() {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
+function updateData(label, data) {
+    //var chart = document.getElementById(chartID);
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
